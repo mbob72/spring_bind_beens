6c94700a24ae844fb0313bbb24cc08e13c12ad8d
Михаил Бобрышев <mbob72@gmail.com>
2020-06-26 20:08:10 +0300

    task have done

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,10 +1,20 @@
 package app.model;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
 
     public Long getTime() {
         return nanoTime;
+    }
+
+    public static void main(String[] args) {
+        Timer timer = new Timer();
+        System.out.println(timer.getTime());
+        System.out.println(timer.getTime());
+        System.out.println(timer.getTime());
     }
 }
